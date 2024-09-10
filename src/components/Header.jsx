80db33f180935405770dbe8effecfd78c06b4577
Ani Maghradze <animaghradze50@gmail.com>
2024-09-10 15:20:11 +0400

    fixed header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export const Header = () => {
     return (
-        <div className='flex justify-between py-3 px-3 bg-black text-white'>
+        <header className='flex fixed w-full justify-between py-3 px-3 bg-black text-white'>
             <a className="text-xl" href="/">Space X</a>
             <nav className='flex gap-3'>
                 <a href="/">Home</a>
@@ -8,6 +8,6 @@ export const Header = () => {
                 <a href="/contact">Contact</a>
                 <a href="/missions">Missions</a>
             </nav>
-        </div>
+        </header>
     )
 }
