c97a1801b91e4286c5401adebfe2f950d4d05055
Ani Maghradze <animaghradze50@gmail.com>
2024-09-13 16:20:08 +0400

    Update Header.jsx
    added styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,14 @@
 export const Header = () => {
     return (
         <header className='w-full z-50 bg-black text-white fixed'>
-            <div className='container mx-auto'>
+            <div className='container mx-auto border-b border-amber-800'>
                 <div className='flex justify-between py-3'>
-                    <a className="text-xl" href="/">Space X</a>
+                    <a className="text-xl cursor-pointer hover:text-amber-800" href="/">Space X</a>
                     <nav className='flex gap-3'>
-                        <a href="/">Home</a>
-                        <a href="/about">About</a>
-                        <a href="/contact">Contact</a>
-                        <a href="/missions">Missions</a>
+                        <a className='hover:text-amber-800' href="/">Home</a>
+                        <a className='hover:text-amber-800' href="/about">About</a>
+                        <a className='hover:text-amber-800' href="/contact">Contact</a>
+                        <a className='hover:text-amber-800' href="/missions">Missions</a>
                     </nav>
                 </div>
             </div>
