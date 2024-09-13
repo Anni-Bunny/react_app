9d28d62fc24e01211fc34aa405edf8d64d4f9b37
Ani Maghradze <animaghradze50@gmail.com>
2024-09-13 15:03:21 +0400

    edited header structure

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,18 @@
 export const Header = () => {
     return (
-        <header className='flex fixed w-full justify-between py-3 px-3 bg-black text-white'>
-            <a className="text-xl" href="/">Space X</a>
-            <nav className='flex gap-3'>
-                <a href="/">Home</a>
-                <a href="/about">About</a>
-                <a href="/contact">Contact</a>
-                <a href="/missions">Missions</a>
-            </nav>
+        <header className='w-full z-50 bg-black text-white fixed'>
+            <div className='container mx-auto'>
+                <div className='flex justify-between py-3'>
+                    <a className="text-xl" href="/">Space X</a>
+                    <nav className='flex gap-3'>
+                        <a href="/">Home</a>
+                        <a href="/about">About</a>
+                        <a href="/contact">Contact</a>
+                        <a href="/missions">Missions</a>
+                    </nav>
+                </div>
+            </div>
         </header>
     )
 }
+
