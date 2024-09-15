99cb6b14c9ff1ff8a51252c92e82c5e640cd3cd3
Ani Maghradze <animaghradze50@gmail.com>
2024-09-15 21:37:09 +0400

    Create Spaceflight.jsx

---
+++ src/pages/Spaceflight.jsx
@@ -0,0 +1,40 @@
+export function Spaceflight() {
+    return (
+        <div className="bg-black">
+            <div className="container mx-auto text-white py-14 space-y-6">
+                <h1 className="text-2xl font-bold text-white">THE EXTRAVEHICULAR ACTIVITY (EVA) SUIT</h1>
+                <img className="mx-auto"
+                     src="https://i0.wp.com/spacenews.com/wp-content/uploads/2024/05/pdawn-evasuits.jpg?resize=1200%2C676&ssl=1"
+                     alt=""/>
+                <p className="text-base/loose">In February 2022, Jared Isaacman and SpaceX announced the Polaris
+                    Program, an effort designed to rapidly advance human spaceflight capabilities, while also supporting
+                    important causes here on Earth.
+
+                    Polaris Dawn, the first of the program’s three human spaceflight missions, is targeted to launch to
+                    orbit no earlier than summer 2024. During the five-day mission, the crew will perform SpaceX’s
+                    first-ever Extravehicular Activity (more commonly known as an EVA or spacewalk) from Dragon, which
+                    will also be the first-ever commercial astronaut spacewalk. This historic milestone will also be the
+                    first time four astronauts will be exposed to the vacuum of space at the same time.
+
+                    Supporting the crew throughout the spacewalk will be SpaceX’s newly-developed EVA suit, an evolution
+                    of the Intravehicular Activity (IVA) suit crews currently wear aboard Dragon human spaceflight
+                    missions. Developed with mobility in mind, SpaceX teams incorporated new materials, fabrication
+                    processes, and novel joint designs to provide greater flexibility to astronauts in pressurized
+                    scenarios while retaining comfort for unpressurized scenarios. The 3D-printed helmet incorporates a
+                    new visor to reduce glare during the EVA in addition to the new Heads-Up Display (HUD) and camera
+                    that provide information on the suit’s pressure, temperature, and relative humidity. The suit also
+                    incorporates enhancements for reliability and redundancy during a spacewalk, adding seals and
+                    pressure valves to help ensure the suit remains pressurized and the crew remains safe.
+
+                    All of these enhancements to the EVA suit are part of a scalable design, allowing teams to produce
+                    and scale to different body types as SpaceX seeks to create greater accessibility to space for all
+                    of humanity.
+
+                    While Polaris Dawn will be the first time the SpaceX EVA suit is used in low-Earth orbit, the suit’s
+                    ultimate destiny lies much farther from our home planet. Building a base on the Moon and a city on
+                    Mars will require the development of a scalable design for the millions of spacesuits required to
+                    help make life multiplanetary.</p>
+            </div>
+        </div>
+    )
+}
