4bf8a18b785c8fec3bd8501488089a6c1960b9db
Ani Maghradze <animaghradze50@gmail.com>
2024-09-15 21:35:57 +0400

    Update Button.jsx

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,6 +3,7 @@ export function Button() {
         className="overflow-hidden group/button relative border border-amber-800 p-2 transition duration-200 flex">
         <span className="invert">LEARN MORE</span>
         <div
-            className="group-hover/button:top-[-100%] left-0 absolute bg-white w-full h-full top-full transition-all duration-600"></div>
+            className="group-hover/button:top-[-100%] left-0 absolute bg-white w-full h-full top-full transition-all duration-600">
+        </div>
     </button>;
 }
