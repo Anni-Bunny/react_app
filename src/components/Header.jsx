77dff16fc51db794871e11a536d20e7522e6b283
Ani Maghradze <animaghradze50@gmail.com>
2024-09-15 21:35:10 +0400

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,6 +1,6 @@
 export const Header = () => {
     return (
-        <header className='w-full z-50 bg-black text-white fixed'>
+        <header className='w-full z-50 bg-black text-white fixed '>
             <div className='container mx-auto border-b border-amber-800'>
                 <div className='flex justify-between py-3'>
                     <a className="text-xl cursor-pointer hover:text-amber-800" href="/">Space X</a>
