60ff616adcee3316907e0a1e8fc56dec63ca7510
Ani Maghradze <animaghradze50@gmail.com>
2024-09-15 14:40:51 +0400

    created Button component

---
+++ src/components/Button.jsx
@@ -0,0 +1,8 @@
+export function Button() {
+    return <button
+        className="overflow-hidden group/button relative border border-amber-800 p-2 transition duration-200 flex">
+        <span className="invert">LEARN MORE</span>
+        <div
+            className="group-hover/button:top-[-100%] left-0 absolute bg-white w-full h-full top-full transition-all duration-600"></div>
+    </button>;
+}
